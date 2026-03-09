8e8ae2cf4f1977faaad44fb385f7c32523fe0a26
darklhg-creator <darklhg@naver.com>
2026-03-09 21:44:29 +0900

    Update start.py

--- start.py
+++ start.py
@@ -3,16 +3,12 @@ import requests
 KRX_API_KEY = "5AB85D9D43EA4FAA9BC1907303BAFDC2C0377C5B"
 
 # 고객예탁금 테스트
-url = "https://openapi.krx.co.kr/contents/COM/GenerateOTP.jspx"
+url = "http://data-dbg.krx.co.kr/svc/apis/sto/stk_bydd_trd"
 params = {
-    "AUTH_KEY": KRX_API_KEY,
-    "bld": "dbms/MDC/STAT/standard/MDCSTAT03901",
-    "name": "fileDown",
-    "filetype": "json"
+    "basDd": "20260306"
 }
-
 headers = {
-    "User-Agent": "Mozilla/5.0"
+    "AUTH_KEY": KRX_API_KEY
 }
 
 res = requests.get(url, params=params, headers=headers, timeout=10)
